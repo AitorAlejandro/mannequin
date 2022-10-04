c39596827d41418781045bc44e7b92c55a8e5b0c
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2022-10-04 19:00:57 +0200

    example 2 - position property

--- scripts/example-2.js
+++ scripts/example-2.js
@@ -1,2 +1,8 @@
 createScene();
 imanol = new Child();
+imanol.position.x = 70;
+imanol.position.z = -180;
+
+aitor = new Child();
+aitor.position.x = -70
+aitor.position.y = -7.7; // 7.7 es el punto cero
