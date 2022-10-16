047df47ccc0053d7f612a684f53c355fb57b6df0
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2022-10-16 19:05:33 +0200

    example 3 - turn

--- scripts/example-3.js
+++ scripts/example-3.js
@@ -7,10 +7,15 @@ imanol.position.z = -50;
 aitor = new Male();
 aitor.position.x = -50;
 
+imanol.turn = 230;
 
-/*
+aitor.turn = 300;
+aitor.torso.turn = 20;
+aitor.pelvis.turn = 10;
+aitor.head.turn = 50;
+
+
 function animate(t) {
-  console.log(t);
-  scene.rotation.y = rad(5 * sin(t / 2));
+  scene.rotation.y = rad(10 * sin(t / 2));
 }
-*/
+
