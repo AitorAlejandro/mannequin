b6db22037073c83812983b4d87f263cf67f0ace4
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2022-10-16 12:39:13 +0200

    example 3 boilerplate

---
+++ scripts/example-3.js
@@ -0,0 +1,16 @@
+createScene();
+
+imanol = new Child();
+imanol.position.x = 50;
+imanol.position.z = -50;
+
+aitor = new Male();
+aitor.position.x = -50;
+
+
+/*
+function animate(t) {
+  console.log(t);
+  scene.rotation.y = rad(5 * sin(t / 2));
+}
+*/
