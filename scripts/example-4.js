3d71e9d3aadb3767f4bc320ec7e9ddb87a2a483a
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2022-10-23 19:15:17 +0200

    example 4 - child greeting

--- scripts/example-4.js
+++ scripts/example-4.js
@@ -1,7 +1,12 @@
 createScene();
 
 imanol = new Child();
+imanol.r_arm.straddle = 90;
+imanol.r_arm.turn = 90
 
 function animate(t) {
-  scene.rotation.y = rad(45 * sin(t / 2));
+  imanol.head.turn = 20 * sin(t / 2 - 20);
+  imanol.torso.tilt = 10 * sin(t / 2 - 20);
+
+  imanol.r_elbow.bend = 45 - 45* sin(t / 2);
 }
